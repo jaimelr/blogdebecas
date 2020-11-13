0ff23e91c40055f2dbf3647aeafe73cece5b1c63
Jaime Loyola <j.loyolarangel@gmail.com>
2020-11-12 22:15:36 -0600

    Create Category seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+['internal', 'external', 'partnership'].each do |category_name|
+  Category.create(name: category_name)
+end
